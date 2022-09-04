719b5077d61ffde8785ac43939062a2675dae30a
cmmerritt <christiane.merritt@gmail.com>
2022-09-03 23:20:00 -0700

    refactor public header using material-ui

--- src/components/app/PublicHeader.jsx
+++ src/components/app/PublicHeader.jsx
@@ -1,17 +1,75 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import styles from './Header.module.css';
+import { Link as RouterLink } from 'react-router-dom';
+import { AppBar, Toolbar, makeStyles, Button } from '@material-ui/core';
+// import styles from './Header.module.css';
 
 const PublicHeader = () => {
-  return <header className={styles.Header}>
-    <div>
+
+  const headersData = [
+    {
+      label: "Events",
+      href: "/events",
+    },
+    {
+      label: "Sign up/Log in",
+      href: "/signup",
+    }
+  ];
+
+  const useStyles = makeStyles(() => ({
+    header: {
+      fontFamily: "Halloween",
+      backgroundColor: "#403d39ff",
+      color: "#eb5e28ff",
+    },
+  }));
+
+  const { header } = useStyles();
+
+  const getMenuButtons = () => {
+    return headersData.map(({ label, href }) => {
+      return (
+        <Button
+          {...{
+            key: label,
+            color: "inherit",
+            to: href,
+            component: RouterLink,
+          }}
+        >
+          {label}
+        </Button>
+      );
+    });
+  };
+
+  const headerDisplay = () => {
+
+    return (
+    <Toolbar>
+      <div>
       <h1>POrtlAnd HalLoweEn EveNt FiNdEr</h1>
-    </div>
-    <nav>
-      <NavLink to="/events">Events list</NavLink>
-      <NavLink to="/signup">Sign up</NavLink>
-    </nav>
-  </header>;
+      </div>
+      {getMenuButtons()}
+    </Toolbar>
+    )
+  };
+
+  return (
+    <header>
+      <AppBar className={header}>{headerDisplay()}</AppBar>
+    </header>
+  );
+
+//   return <header className={styles.Header}>
+//     <div>
+//       <h1>POrtlAnd HalLoweEn EveNt FiNdEr</h1>
+//     </div>
+//     <nav>
+//       <NavLink to="/events">Events list</NavLink>
+//       <NavLink to="/signup">Sign up</NavLink>
+//     </nav>
+//   </header>;
 };
 
 export default PublicHeader;
