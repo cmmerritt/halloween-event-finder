a04a1222de9b797aaefe01eb5f0d17e49d51bbb1
cmmerritt <christiane.merritt@gmail.com>
2022-09-03 23:37:03 -0700

    fix dependency issues for material-ui

--- src/components/app/PublicHeader.jsx
+++ src/components/app/PublicHeader.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import { AppBar, Toolbar, makeStyles, Button } from '@material-ui/core';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
 // import styles from './Header.module.css';
 
 const PublicHeader = () => {
@@ -16,15 +18,13 @@ const PublicHeader = () => {
     }
   ];
 
-  const useStyles = makeStyles(() => ({
+  const styles = {
     header: {
       fontFamily: "Halloween",
       backgroundColor: "#403d39ff",
       color: "#eb5e28ff",
     },
-  }));
-
-  const { header } = useStyles();
+  };
 
   const getMenuButtons = () => {
     return headersData.map(({ label, href }) => {
@@ -57,7 +57,7 @@ const PublicHeader = () => {
 
   return (
     <header>
-      <AppBar className={header}>{headerDisplay()}</AppBar>
+      <AppBar className={styles}>{headerDisplay()}</AppBar>
     </header>
   );
 
