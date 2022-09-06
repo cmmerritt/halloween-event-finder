37e2b1f2939dbfc9a3236fe861838ebf78603d94
cmmerritt <christiane.merritt@gmail.com>
2022-09-05 23:56:04 -0700

    set up for responsive header

--- src/components/app/PublicHeader.jsx
+++ src/components/app/PublicHeader.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -6,6 +6,27 @@ import Button from '@mui/material/Button';
 import styled from '@emotion/styled';
 
 const PublicHeader = () => {
+
+  const [state, setState] = useState({
+    mobileView: false,
+  })
+
+  const { mobileView } = state;
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+        ? setState((prevState) => ({ ...prevState, mobileView: true }))
+        : setState((prevState) => ({ ...prevState, mobileView: false }));
+    };
+
+    setResponsiveness();
+    window.addEventListener("resize", () => setResponsiveness());
+
+    return () => {
+      window.removeEventListener("resize", () => setResponsiveness());
+    }
+  }, []);
 
   const headersData = [
     {
@@ -39,6 +60,12 @@ const PublicHeader = () => {
     font-family: "Halloween";
     background-color: #403d39ff;
     color: #eb5e28ff;
+    display: flex;
+    justify-content: space-between;
+  `
+
+  const ButtonDiv = styled.div`
+    padding-left: 500px;
   `
 
   const headerDisplay = () => {
@@ -48,7 +75,9 @@ const PublicHeader = () => {
         <div>
         <h1>POrtlAnd HalLoweEn EveNt FiNdEr</h1>
         </div>
-        {getMenuButtons()}
+        <ButtonDiv>
+          {getMenuButtons()}
+        </ButtonDiv>
       </Toolbar>
     </Div>
     )
@@ -56,7 +85,7 @@ const PublicHeader = () => {
 
   return (
     <header>
-      <AppBar className>{headerDisplay()}</AppBar>
+      <AppBar>{headerDisplay()}</AppBar>
     </header>
   );
 
