679bd8fe0253b911951c27d8413ff38a52372609
cmmerritt <christiane.merritt@gmail.com>
2022-09-06 23:24:37 -0700

    try netlify.toml file

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -20,7 +20,6 @@ export default function App() {
         <UserContextProvider supabaseClient={supabaseClient}>
           <Header />
           <Routes>
-            <Route path="/index.html" element={<EventFeed />} />
             <Route path="/events" element={<EventFeed />} />
             <Route path="/events/:id" element={<EventDetail />} />
             <Route path="/signup" element={<SignUp />} />
