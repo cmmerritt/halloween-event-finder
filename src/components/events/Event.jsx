e12569d9b2d0cac0d58d84a6eb346e46779d4d5e
cmmerritt <christiane.merritt@gmail.com>
2022-09-07 21:20:06 -0700

    date and time working properly in Safari and iOS

--- src/components/events/Event.jsx
+++ src/components/events/Event.jsx
@@ -4,15 +4,25 @@ import { format } from "date-fns";
 import PropTypes from "prop-types";
 
 const Event = ({ id, title, description, date, time, location, address, url, isFree, price, category }) => {
-  const formattedDate = format(new Date(date), 'cccc, MMMM d, yyy');
-  const datePlusTime = date + ' ' + time;
-  const formattedTime = format(new Date(datePlusTime), 'h:mm bbbb')
+
+  const formattedDate = format((new Date(date)), 'cccc, MMMM d, yyy');
+
+  let formattedTime;
+
+  const slicedTimeHoursOnly = time.slice(0, 2);
+
+  if(slicedTimeHoursOnly > 13) {
+    formattedTime = (slicedTimeHoursOnly - 12) + ":00 PM"
+  } else {
+    formattedTime = slicedTimeHoursOnly + ":00 AM"
+  }
   
   return (
   <>
     <Link to={`/events/${id}`}>{title}</Link>
     <p>{description}</p>
-    <p>{formattedDate} at {formattedTime}</p>
+    <p>{formattedDate}</p>
+    <p>{formattedTime}</p>
     <p>{location}, {address}</p>
     <p><a href={url}>Link to event</a></p>
     <p>Is this event free? {isFree}</p>
